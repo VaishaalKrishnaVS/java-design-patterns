edc5c7ee70c0106d4976c2a23366d02af546a00b
hyoongshin_acksal <vaishaalinc@gmail.com>
2025-02-08 11:01:25 +0530

    added delegation principle

---
+++ src/main/java/designprinciples/delegationprinciple/Ninja.java
@@ -0,0 +1,5 @@
+package designprinciples.delegationprinciple;
+
+interface Ninja {
+    void executeMission(String mission);
+}
---
+++ src/main/java/designprinciples/delegationprinciple/ShinobiDelegation.java
@@ -0,0 +1,8 @@
+package designprinciples.delegationprinciple;
+
+public class ShinobiDelegation {
+    public static void main(String[] args) {
+        Hokage hokage = new Hokage();
+        hokage.assignMission("Protect the Hidden Leaf Village");
+    }
+}
